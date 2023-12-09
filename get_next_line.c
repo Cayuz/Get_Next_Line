862ab90fe30fd6da982f77f3b0921a7f4b6a07fe
Caya Van Vliet <cavan-vl@f0r4s5.codam.nl>
2023-12-09 16:10:00 +0100

    elp

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,50 @@
 /*   By: cavan-vl <cavan-vl@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/23 14:01:48 by cavan-vl      #+#    #+#                 */
-/*   Updated: 2023/12/09 12:44:33 by cavan-vl      ########   odam.nl         */
+/*   Updated: 2023/12/09 15:19:56 by cavan-vl      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	clean_up(t_list **list)
+{
+	t_list	*clean_node;
+	t_list	*last_node;
+	int		i;
+	int		j;
+	char	*buffer;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	last_node = ft_lstlast(*list);
+	if (!buffer || !last_node)
+		return ;
+	i = 0;
+	j = 0;
+	while (last_node->line[i] && last_node->line[i] != '\n')
+		i++;
+	while (last_node->line[i] && last_node->line[++i])
+	{
+		buffer[j] = last_node->line[i];
+		j++;
+	}
+	clean_node->line = buffer;
+	clean_node->next = NULL;
+}
 
 char	*fetch_line(t_list *list)
 {
 	int		len_to_newline;
 	char	*line;
 
+	if (!list)
+		return (NULL);
 	len_to_newline = find_newline(list);
 	line = malloc(len_to_newline + 1);
-}
-
-void	append_node(t_list **list, char *buffer)
-{
-	t_list	*last_node;
-	t_list	*new_node;
-
-	new_node = malloc(sizeof(t_list));
-	last_node = ft_lstlast(&list);
-	if (!last_node)
-		*list = new_node;
-	else
-		last_node->next = new_node;
-	new_node->line = buffer;
-	new_node->next = NULL;
-}
-
-int	find_newline(t_list *list)
-{
-	int	i;
-
-	i = 0;
-	while (list->line[i])
-	{
-		if (list->line[i] == '\n')
-			return (i);
-		i++;
-	}
-	return (0);
+	if (!line)
+		return (NULL);
+	copy_string(list, line);
+	return (line);
 }
 
 void	create_list(t_list **list, int fd)
@@ -55,6 +57,8 @@ void	create_list(t_list **list, int fd)
 	char	*buffer;
 	int		chars;
 
+	if(!list)
+		return ;
 	while (!find_newline(*list))
 	{
 		buffer = malloc(BUFFER_SIZE + 1);
@@ -82,5 +86,6 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	create_list(&list, fd);
-	// fetch new line
+	line = fetch_line(list);
+	clean_up(&list);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,37 +6,85 @@
 /*   By: cavan-vl <cavan-vl@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/30 16:16:37 by cavan-vl      #+#    #+#                 */
-/*   Updated: 2023/12/07 19:58:23 by cavan-vl      ########   odam.nl         */
+/*   Updated: 2023/12/09 13:54:58 by cavan-vl      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*ptr;
-	size_t	i;
-
-	ptr = malloc(nmemb * size);
-	if (!ptr || ptr == NULL)
-		return(NULL);
-	i = 0;
-	while (i < (nmemb * size))
-	{
-		((char *)ptr)[i] = '\0';
-		i++;
-	}
-	return(ptr);
-}
-
 t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*temp;
 
-	if (lst == NULL)
+	if (!lst)
 		return (NULL);
 	temp = lst;
 	while (temp->next)
 		temp = temp->next;
 	return (temp);
+}
+
+void	copy_string(t_list	*list, char *line)
+{
+	int	i;
+	int	j;
+
+	if (!list)
+		return ;
+	j = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->line[i])
+		{
+			if (list->line[i] == '\n')
+			{
+				line[j++] = '\n';
+				line[j] = '\0';
+				return ;
+			}
+			line[j++] = list->line[i++];
+		}
+		list = list->next;
+	}
+	line[j] = '\0';
+}
+void	append_node(t_list **list, char *buffer)
+{
+	t_list	*last_node;
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node || !list)
+		return ;
+	last_node = ft_lstlast(&list);
+	if (!last_node)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+	new_node->line = buffer;
+	new_node->next = NULL;
+}
+
+int	find_newline(t_list *list)
+{
+	int	i;
+	int	length;
+
+	if (!list)
+		return (NULL);
+	length = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->line[i])
+		{
+			if (list->line[i] == '\n')
+				return (length);
+		length++;
+		i++;
+		}
+		list = list->next;
+	}
+	return (0);
 }
